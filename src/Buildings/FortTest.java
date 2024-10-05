d9e38d109041efb1f385153d83b41f23c1164009
M <markpackham1@gmail.com>
2024-10-05 23:40:15 +0100

    Explain house id test

--- src/Buildings/FortTest.java
+++ src/Buildings/FortTest.java
@@ -25,6 +25,7 @@ class FortTest {
 
     @Test
     void testGetHouseID() {
+        // HouseID uses a UUID (ergo random) so we can only test to see if it is null
         assertNotNull(fort.getHouseID(), "HouseID must not be null");
     }
 
