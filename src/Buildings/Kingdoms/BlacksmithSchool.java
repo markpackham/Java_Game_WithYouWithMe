4da5b1447df46b8aea78619787ecb0225bc5fcfe
M <markpackham1@gmail.com>
2024-10-07 17:40:59 +0100

    Sort out knight printing

--- src/Buildings/Kingdoms/BlacksmithSchool.java
+++ src/Buildings/Kingdoms/BlacksmithSchool.java
@@ -3,8 +3,8 @@ package Buildings.Kingdoms;
 import Buildings.Building;
 
 // Belong to a Kingdom
-public class Workshop extends Building {
-    public Workshop(String buildingName, int buildingYearBuilt, int windows, int doors) {
+public class BlacksmithSchool extends Building {
+    public BlacksmithSchool(String buildingName, int buildingYearBuilt, int windows, int doors) {
         super(buildingName, buildingYearBuilt, windows, doors);
     }
 }
--- src/Territory/Kingdom.java
+++ src/Territory/Kingdom.java
@@ -45,7 +45,7 @@ public class Kingdom extends Territory {
         populateTerritory(numberOfKnights);
 
         // Adding blacksmiths
-        System.out.print("How many smiths live in your territory?: ");
+        System.out.print("\nHow many smiths live in your territory?: ");
         int numberOfSmiths = Integer.parseInt(scanner.nextLine());
         populateTerritorySmiths(numberOfKnights);
 
@@ -56,41 +56,43 @@ public class Kingdom extends Territory {
     }
 
     private void populateTerritory(int iterations) {
-        for (int i = 0; i < iterations; i++) {
-            System.out.print("\nEnter Knight first name: ");
-            String fName = scanner.nextLine();
-            System.out.print("Enter Knight last name: ");
-            String sName = scanner.nextLine();
-            System.out.print("Enter Knight age: ");
-            int age = Integer.parseInt(scanner.nextLine());
-            Knight knight = new Knight(fName, sName, age);
-            this.villagers.add(knight);
 
-            System.out.print("Enter Fort to station Knight at: ");
-            String fortName = scanner.nextLine();
-            // Put Knight in a fort
-            Fort fort = findOrCreateFort(fortName);
-            fort.addKnight(knight);
-        }
+            for (int i = 0; i < iterations; i++) {
+                System.out.print("\nEnter Knight first name: ");
+                String fName = scanner.nextLine();
+                System.out.print("Enter Knight last name: ");
+                String sName = scanner.nextLine();
+                System.out.print("Enter Knight age: ");
+                int age = Integer.parseInt(scanner.nextLine());
+                Knight knight = new Knight(fName, sName, age);
+                this.villagers.add(knight);
+
+                System.out.print("Enter Fort to station Knight at: ");
+                String fortName = scanner.nextLine();
+                // Put Knight in a fort
+                Fort fort = findOrCreateFort(fortName);
+                fort.addKnight(knight);
+            }
     }
 
     private void populateTerritorySmiths(int iterations) {
-        for (int i = 0; i < iterations; i++) {
-            System.out.print("\nEnter Blacksmith first name: ");
-            String fName = scanner.nextLine();
-            System.out.print("Enter Blacksmith last name: ");
-            String sName = scanner.nextLine();
-            System.out.print("Enter Blacksmith age: ");
-            int age = Integer.parseInt(scanner.nextLine());
-            Blacksmith blacksmith = new Blacksmith(fName, sName, age);
-            this.villagers.add(blacksmith);
 
-            System.out.print("Enter Forge to station smith at: ");
-            String forgeName = scanner.nextLine();
-            // Put Knight in a fort
-            Forge forge = findOrCreateForge(forgeName);
-            forge.addSmith(blacksmith);
-        }
+            for (int i = 0; i < iterations; i++) {
+                System.out.print("\nEnter Blacksmith first name: ");
+                String fName = scanner.nextLine();
+                System.out.print("Enter Blacksmith last name: ");
+                String sName = scanner.nextLine();
+                System.out.print("Enter Blacksmith age: ");
+                int age = Integer.parseInt(scanner.nextLine());
+                Blacksmith blacksmith = new Blacksmith(fName, sName, age);
+                this.villagers.add(blacksmith);
+
+                System.out.print("Enter Forge to station smith at: ");
+                String forgeName = scanner.nextLine();
+                // Put Blacksmith in a fort
+                Forge forge = findOrCreateForge(forgeName);
+                forge.addSmith(blacksmith);
+            }
     }
 
     // Build forge
@@ -99,6 +101,36 @@ public class Kingdom extends Territory {
         for (Forge forge : forges) {
             if (forge.getBuildingName().equals(forgeName)) {
                 return forge;
+            }
+        }
+
+        System.out.print("Enter year fort built (enter nothing to use current year): ");
+        String input = scanner.nextLine();
+        if (!input.isEmpty()) {
+            try {
+                buildingYearBuilt = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Using default year: " + buildingYearBuilt);
+            }
+        }
+
+        System.out.print("Enter number of windows fort has (default is 10): ");
+        input = scanner.nextLine();
+        if (!input.isEmpty()) {
+            try {
+                windows = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Using default windows: " + windows);
+            }
+        }
+
+        System.out.print("Enter number of doors fort has (default is 2): ");
+        input = scanner.nextLine();
+        if (!input.isEmpty()) {
+            try {
+                doors = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Using default doors: " + doors);
             }
         }
 
@@ -156,6 +188,10 @@ public class Kingdom extends Territory {
             // Print Knight
             if (v.getClass() == Knight.class) {
                 ((Knight) v).print();
+            }
+
+            if (v.getClass() == Blacksmith.class) {
+                ((Blacksmith) v).print();
             }
         }
     }
