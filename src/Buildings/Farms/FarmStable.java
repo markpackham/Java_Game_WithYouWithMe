9241259a57e43db3bf24fa5d7eb03b69ad27c8f0
M <markpackham1@gmail.com>
2024-10-08 09:42:25 +0100

    Get horse stable numbers chosen by day & demo enhanced switch statement

--- src/Buildings/Farms/FarmStable.java
+++ src/Buildings/Farms/FarmStable.java
@@ -1,10 +1,34 @@
 package Buildings.Farms;
 
 import Buildings.Building;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 // Belongs to Farmland
 public class FarmStable extends Building {
+
+    private int horseNumbers;
+
     public FarmStable(String buildingName, int buildingYearBuilt, int windows, int doors) {
         super(buildingName, buildingYearBuilt, windows, doors);
+        horseNumbers = this.getHorseNumbers();
+    }
+
+    // Horse numbers in stable changes based on what day of the week the player plays
+    public int getHorseNumbers() {
+        DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek();
+
+        // Demo use of an enhanced switch statement with arrows
+        horseNumbers = switch (dayOfWeek) {
+            case MONDAY -> 5;
+            case TUESDAY -> 6;
+            case WEDNESDAY -> 7;
+            case THURSDAY -> 8;
+            case FRIDAY -> 9;
+            case SATURDAY -> 10;
+            case SUNDAY -> 11;
+        };
+
+        return horseNumbers;
     }
 }
