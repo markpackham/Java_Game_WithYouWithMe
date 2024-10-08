cf70b7376aa0575b4c0a51d7cf9850fd121201f0
M <markpackham1@gmail.com>
2024-10-08 19:31:39 +0100

    Add Farmland constructor

--- src/Territory/Farmland.java
+++ src/Territory/Farmland.java
@@ -1,12 +1,34 @@
 package Territory;
 
+import Buildings.Farms.FarmCattleBarn;
+import Buildings.Farms.FarmSilo;
+import Buildings.Farms.FarmStable;
 import Villagers.Villager;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 // Farmland can host silos with farmers but not have forts with knights and forges with blacksmiths
 public class Farmland extends Territory {
+
+    private final Scanner scanner;
+    private List<FarmCattleBarn> farmCattleBarns;
+    private List<FarmSilo> farmSilos;
+    private List<FarmStable> farmStables;
+
     public Farmland(String name, List<Villager> villagers) {
         super(name, villagers);
+
+        this.scanner = new Scanner(System.in);
+        this.farmCattleBarns = new ArrayList<>();
+        this.farmSilos = new ArrayList<>();
+        this.farmStables = new ArrayList<>();
+
+        initTerritoryFarm();
     }
+
+    private void initTerritoryFarm() {
+    }
+
 }
