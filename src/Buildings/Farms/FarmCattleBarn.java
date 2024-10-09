06143ae212ce934786cf50105236f9ce7f6a0a72
M <markpackham1@gmail.com>
2024-10-09 11:09:33 +0100

    Demo use of modulus and day of month for animal  in barns

--- src/Buildings/Farms/FarmCattleBarn.java
+++ src/Buildings/Farms/FarmCattleBarn.java
@@ -2,9 +2,30 @@ package Buildings.Farms;
 
 import Buildings.Building;
 
+import java.time.LocalDate;
+
 // Belongs to Farmland
 public class FarmCattleBarn extends Building {
+
+    String animals;
+
     public FarmCattleBarn(String buildingName, int buildingYearBuilt, int windows, int doors) {
         super(buildingName, buildingYearBuilt, windows, doors);
+    }
+
+    // The animals stored in the barn chosen based on the day of the month
+    public String getAnimals() {
+        int monthDay = LocalDate.now().getDayOfMonth();
+
+        if (monthDay == 1) {
+            animals = "goats";
+        }
+        // Demo use of modulus, if even animals will be pigs otherwise cows
+        else if (monthDay % 2 == 0) {
+            animals = "pigs";
+        } else {
+            animals = "cows";
+        }
+        return animals;
     }
 }
